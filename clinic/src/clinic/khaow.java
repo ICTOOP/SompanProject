8fb285aaaa2c8ec605bf73b1d80423cad3782e91
Administrator <Administrator@10.240.9.67>
2014-07-02 09:43:15 +0700

    สร้างคลาส

---
+++ clinic/src/clinic/khaow.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clinic;
+
+/**
+ *
+ * @author Administrator
+ */
+public class khaow {
+    String name ="Arporn Krumram" ;
+    String stuid ="55021463" ;
+}
