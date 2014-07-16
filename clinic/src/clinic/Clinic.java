708f33941d472a2bcc8c1eed764c2933dacd1f05
Administrator <Administrator@10.240.9.58>
2014-07-16 09:40:29 +0700

    test

--- clinic/src/clinic/Clinic.java
+++ clinic/src/clinic/Clinic.java
@@ -17,6 +17,8 @@ public class Clinic {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       Home  homeframe=new Home();
+        homeframe.setVisible(true);
     }
     
 }
