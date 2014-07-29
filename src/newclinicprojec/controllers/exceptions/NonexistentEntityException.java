a4b6a0789c4bf62ab540b0ddbdb4713f470c5a8e
Johm_93 <Johm_93@10.10.198.73>
2014-07-28 23:48:42 -0700

    Create new project

---
+++ src/newclinicprojec/controllers/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package newclinicprojec.controllers.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
---
+++ src/newclinicprojec/controllers/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package newclinicprojec.controllers.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
